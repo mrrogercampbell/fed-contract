508e06356ad4d7a939479f35d1c606b708331908
Roger Campbell II <rc1336@gmail.com>
2019-04-06 16:30:25 -0400

    commiting before pulling

--- fed_contract_app/admin.py
+++ fed_contract_app/admin.py
@@ -6,13 +6,3 @@ from .models import Keyword
 admin.site.register(Result)
 admin.site.register(Profile)
 admin.site.register(Keyword)
-
-# Register your models here.
-from django.contrib import admin
-from .models import Result
-from .models import Profile
-from .models import Keyword
-
-admin.site.register(Result)
-admin.site.register(Profile)
-admin.site.register(Keyword)

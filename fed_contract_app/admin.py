0f185246cfb54071c83649f239264e784924fcef
Courtney Tyler <courtneytyler7@gmail.com>
2019-04-06 15:56:36 -0400

    add models to admin file

--- fed_contract_app/admin.py
+++ fed_contract_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Result
+from .models import Profile
+from .models import Keyword
+
+admin.site.register(Result)
+admin.site.register(Profile)
+admin.site.register(Keyword)
